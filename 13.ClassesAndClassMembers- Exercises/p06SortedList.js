d2303a91a0b0f22350cda912cdc31e1abc9d8677
RayaIlieva <rajcheto@gmail.com>
2018-03-02 17:33:04 +0200

    new

---
+++ 13.ClassesAndClassMembers- Exercises/p06SortedList.js
@@ -0,0 +1,33 @@
+class SortedList {
+    constructor() {
+        this.sortedList = [];
+        this.size = 0;
+    }
+
+    add(T) {
+        this.sortedList.push(T);
+        this.size++;
+        this.sortedList = this.sortedList.sort((a, b) => a - b);
+    }
+    remove(index) {
+        if (this.size === 0) {
+            throw new Error('List is empty!');
+        }
+        if (index < 0 || index > this.sortedList.length - 1) {
+            throw new RangeError('Index out of Range!');
+        }
+
+        this.sortedList.splice(index, 1);
+        this.size--;
+    }
+    get(index) {
+        if (this.size === 0) {
+            throw new Error('List is empty!');
+        }
+        if (index < 0 || index > this.sortedList.length - 1) {
+            throw new RangeError('Index out of Range!');
+        }
+
+        return this.sortedList[index];
+    }
+}
---
+++ 13.ClassesAndClassMembers- Exercises/p09CustomForm.js
@@ -0,0 +1,91 @@
+let result = (() => {
+    class Textbox {
+        constructor(selector, regex) {
+            this._elements = $(selector);
+            this.value = $(this._elements[0]).val();
+            this._invalidSymbols = regex;
+            this.onInput();
+        }
+
+        get elements() {
+            return this._elements;
+        }
+
+        get value() {
+            return this._value;
+        }
+
+        set value(value) {
+            this._value = value;
+            for (let element of this.elements) {
+                $(element).val(value);
+            }
+        }
+
+        onInput() {
+            this.elements.on('input', (event) => {
+                this.value = $(event.target).val();
+            });
+        }
+
+        isValid() {
+            return !this._invalidSymbols.test(this.value);
+        }
+    }
+
+    class Form {
+        constructor() {
+            this._element = $('<div>').addClass('form');
+            this.textboxes = arguments;
+        }
+
+        get textboxes() {
+            return this._textboxes;
+        }
+
+        set textboxes(args) {
+            for (let argument of args) {
+                if (!argument instanceof Textbox) {
+                    throw new Error('The parameter is not a instance of Texbox!')
+                }
+            }
+
+            this._textboxes = args;
+
+            for (let textbox of this._textboxes) {
+                for (let element of textbox._elements) {
+                    this._element.append($(element));
+                }
+            }
+        }
+
+        submit() {
+            let isAllValid = true;
+
+            for (let textbox of this._textboxes) {
+                if (textbox.isValid()) {
+                    for (let element of textbox._elements) {
+                        $(element).css('border', '2px solid green');
+                    }
+                } else {
+                    for (let element of textbox._elements) {
+                        $(element).css('border', '2px solid red');
+                    }
+
+                    isAllValid = false;
+                }
+            }
+
+            return isAllValid;
+        }
+
+        attach(selector) {
+            $(selector).append(this._element);
+        }
+    }
+
+    return {
+        Textbox: Textbox,
+        Form: Form
+    }
+})();

25cd07803c042238a94e67bd1ad6133a8a1fd144
RayaIlieva <rajcheto@gmail.com>
2018-03-03 15:07:13 +0200

    exams added

---
+++ 16.Exam- 31.10.2016/p03StormWatcher.js
@@ -0,0 +1,36 @@
+class Record{
+
+    constructor(temperature, humidity, pressure, windSpeed){
+
+        this.id = Record.getId();
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.windSpeed = windSpeed;
+
+    }
+
+    toString(){
+        let status = '';
+        if(this.temperature < 20 && (this.pressure < 700 || this.pressure > 900) && this.windSpeed > 25){
+            status = 'Stormy';
+        }
+        else{
+            status = 'Not stormy';
+        }
+
+
+        return `Reading ID: ${this.id}
+        Temperature: ${this.temperature}*C
+        Relative Humidity: ${this.humidity}%
+        Pressure: ${this.pressure}hpa
+        Wind Speed: ${this.windSpeed}m/s
+        Weather: ${status}`
+    }
+
+    static getId(){
+        if (Record.nextId === undefined)
+            Record.nextId = 0;
+        return Record.nextId++;
+    }
+}
